a2b69ec426df4aa835feccc10833b51ce606fb92
Sara Taylor <sara.swifti@gmail.com>
2021-04-14 21:29:02 -0400

    updated manifest and service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,64 +1,75 @@
 const FILES_TO_CACHE = [
-   '/',
-   '/index.html',
-   '/db.js',
-   '/index.js',
-   '/style.css',
-   './icons/icon-192x192.png',
-   './icons/icon-512x512.png',
-   './manifest.webmanifest'
+    '/',
+    '/index.html',
+    '/db.js',
+    '/index.js',
+    '/style.css',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+    './manifest.webmanifest'
 
-  ];
+];
 
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const DATA_CACHE = 'data-cache-v1';
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
     );
-  });
+});
 
 
-  // The activate handler takes care of cleaning up old caches.
+// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', (event) => {
     const currentCaches = [PRECACHE, RUNTIME];
     event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
             })
-          );
-        })
-        .then(() => self.clients.claim())
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
     );
-  });
-  self.addEventListener('fetch', (event) => {
+});
+self.addEventListener('fetch', (event) => {
     if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
-        })
-      );
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
     }
-  });
-  
+});
+//   //adding even respond not sure about this one
+event.respondWith(
+    fetch(event.request).catch(() => {
+        return caches.match(event.request).then((response) => {
+            if (response) { return response }
+            else if (event.request.headers("accept").includes("text/html")) {
+                return caches.match("/")
+            }
+        })
+    })
+)
